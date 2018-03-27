09bd570d57bda52f6eb821da305634cc7278cf41
maxlero <maxlero@yandex.ru>
2018-03-27 22:32:47 +0300

    doc file updated

--- Lab_4-5_Orel.py
+++ Lab_4-5_Orel.py
@@ -2,14 +2,14 @@
     Автор: Орел Максим
     Группа: КБ-161
     Вариант: 11
-    Дата создания: 28/02/2018
+    Дата создания: 27/03/2018
     Python Version: 3.6
 """
-import warnings
 
-import matplotlib.pyplot as plt
-import numpy as np
 import sys
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.misc import derivative
 
 # Constants
@@ -20,7 +20,7 @@ START_Y = -10
 END_Y = 10
 
 
-# Функции
+# неявные функции
 def f1(x, y):
     return 2 * y + np.cos(x) - 2
 
@@ -29,6 +29,7 @@ def f2(x, y):
     return np.sin(y + 1) - x - 1.2
 
 
+# явные функции
 def _f1(x):
     return 1 - np.cos(x) / 2
 
@@ -37,7 +38,7 @@ def _f2(y):
     return np.sin(y + 1) - 1.2
 
 
-# 0 - means derivative by x, 1 - by y
+# 0 - means derivative with respect to x, 1 - means derivative with respect to y
 def diff_f(func, var=0, point=None):
     if point is None:
         point = []
@@ -50,6 +51,7 @@ def diff_f(func, var=0, point=None):
     return derivative(wraps, point[var], dx=1e-6)
 
 
+# Строит графики функций
 def build_function_xy(func, legend="", colors="black", di=0.01, x_from=START_X, x_to=END_X, y_from=START_Y,
                       y_to=END_Y):
     plt.axis([START_X, END_X, START_Y, END_Y])
@@ -78,19 +80,48 @@ def show_plot():
     plt.show()
 
 
-def iteration(x0, y0, __f1, __f2, n):
+def print_result(x, y, f1, f2, n):
+    temp_x_e = str("%.2e" % x)
+    temp_x_f = str("%.6f" % x)
+    temp_y_e = str("%.2e" % y)
+    temp_y_f = str("%.6f" % y)
+
+    temp_iterations = str(n)
+
+    temp_f1_x_e = str("%.2e" % f1)
+    temp_f1_x_f = str("%.6f" % f1)
+    temp_f2_x_e = str("%.2e" % f2)
+    temp_f2_x_f = str("%.6f" % f2)
+
+    print(
+        ("Корень системы нелинейных уравнений: x = {0} ({1}), y = {2} ({3})\n" +
+         "Найдены за {4} итерации(ий) \n" +
+         "Функции в этих точках: f1(x,y) = {5} ({6}) <= {9}  (точность); f2(x,y) = {7} ({8}) <= {9} (точность)").format(
+            temp_x_e, temp_x_f, temp_y_e, temp_y_f,
+            temp_iterations,
+            temp_f1_x_e, temp_f1_x_f, temp_f2_x_e, temp_f2_x_f,
+            accuracy
+        )
+    )
+
+
+def iteration_space_station(x0, y0, __f1, __f2, n):
     if abs(f1(x0, y0)) < accuracy and abs(f2(x0, y0)) < accuracy:
-        return [x0, y0, f1(x0, y0), f2(x0, y0), n]
+        print("_______________________________________________________________________________________________________")
+        print("Метод итерации:")
+        print_result(x0, y0, f1(x0, y0), f2(x0, y0), n)
     else:
         y = __f1(x0)
         x = __f2(y0)
 
-        return iteration(x, y, __f1, __f2, n + 1)
+        return iteration_space_station(x, y, __f1, __f2, n + 1)
 
 
 def newton_piston(x0, y0, _f1, _f2, n):
     if abs(f1(x0, y0)) < accuracy and abs(f2(x0, y0)) < accuracy:
-        return [x0, y0, f1(x0, y0), f2(x0, y0), n]
+        print("_______________________________________________________________________________________________________")
+        print("Метод Ньютона:")
+        print_result(x0, y0, f1(x0, y0), f2(x0, y0), n)
     else:
 
         detJ = diff_f(f1, 0, [x0, y0]) * diff_f(f2, 1, [x0, y0]) - diff_f(f1, 1, [x0, y0]) * diff_f(f2, 0, [x0, y0])
@@ -118,13 +149,13 @@ if __name__ == "__main__":
     show_plot()
 
     try:
-        print(iteration(0, 0, _f1, _f2, 0))
+        iteration_space_station(0, 0, _f1, _f2, 0)
     except Exception as e:
         print(e)
 
     print()
 
     try:
-        print(newton_piston(-1, -1, f1, f2, 0))
+        newton_piston(-1, -1, f1, f2, 0)
     except Exception as e:
         print(e)
